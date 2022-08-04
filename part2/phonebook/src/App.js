a050e02130887444d24c1526326b77590919e88f
Damqn <stu2101321082@uni-plovdiv.bg>
2022-08-04 14:59:21 +0300

    phonebook step one

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,48 @@
+import { useState } from 'react'
+import Person from './components/Person'
+
+const App = () => {
+
+    const [persons, setPersons] = useState([ ])
+    const [newName, setNewName] = useState(' ')
+
+    const addNumber = (event) => {
+        event.preventDefault();
+        console.log('button clicked', event.target);
+
+        const personObject = {
+            name: newName,
+            date: new Date().toISOString(),
+            important: Math.random() < 0.5,
+        }
+        console.log("personObject is: ", personObject);
+
+        setPersons(persons.concat(personObject));
+        setNewName(" ")
+    }
+
+    const handleNumberChange = (event) => {
+        console.log(event.target.value)
+        setNewName(event.target.value)
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={addNumber}>
+                <input
+                    value={newName}
+                    onChange={handleNumberChange}
+                />
+            <button type='submit'>save</button>
+            </form>
+            <h2>Numbers</h2>
+            <ul>
+                {persons.map(person => <Person key={person.name} person={person} />)}
+            </ul>
+        </div>
+
+    )
+}
+
+export default App
---
+++ part2/phonebook/src/components/Person.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Person = (props) => {
+    return (
+        <li>{props.person.name}</li>
+    )
+}
+
+export default Person

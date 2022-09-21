ff72ad7603912218da155729e82204f9bf9068c9
Damqn <stu2101321082@uni-plovdiv.bg>
2022-09-21 12:42:48 +0300

    bloglist step 4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
     return 1;
 }
 
+const totalLikes = (blogPosts) => {
+    return blogPosts
+        .map(blog => blog.likes)
+        .reduce((initial, next) => initial + next, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }

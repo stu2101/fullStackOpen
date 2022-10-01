2f340d512e1770ca02b2222be05bd8c2930fb9f6
Damqn <stu2101321082@uni-plovdiv.bg>
2022-10-01 12:56:20 +0300

    bloglist step 15

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -2,7 +2,10 @@ const config = require('./utils/config')
 const express = require('express')
 const app = express()
 const cors = require('cors')
+
 const blogRouter = require('./controllers/blogs')
+const userRouter = require("./controllers/users")
+
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -18,7 +21,10 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
+
 app.use('/api/blogs', blogRouter)
+app.use("/api/users", userRouter)
+
 app.use(middleware.errorHandler)
 
 module.exports = app;
---
+++ part4/bloglist/controllers/users.js
@@ -0,0 +1,38 @@
+const bcryptjs = require('bcryptjs');
+const userRouter = require("express").Router()
+const User = require("../models/user")
+
+userRouter.get("/", async (request, response) => {
+    const users = await User
+        .find({})
+        .populate("blogs", { title: 1, author: 1 })
+
+    response.json(users);
+})
+
+userRouter.post("/", async (request, response) => {
+    const { username, name, password } = request.body;
+
+    // const existingUser = await User.findOne({ username })
+    // if (existingUser) {
+    //     return response.status(400).json({
+    //         error: "username must be unique"
+    //     });
+    // }
+
+    const saltRounds = 10;
+    const passwordHash = await bcryptjs.hash(password, saltRounds);
+
+    const newUser = new User({
+        username,
+        name,
+        passwordHash
+    })
+
+    const savedUser = await newUser.save();
+
+    response.status(201).json(savedUser);
+
+})
+
+module.exports = userRouter;
